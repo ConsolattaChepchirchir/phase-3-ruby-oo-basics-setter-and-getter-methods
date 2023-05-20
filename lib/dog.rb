d12fb8443d4d4d3bb62b0b797ab950dcdeecc4ff
ConsolattaChepchirchir <consochep97@gmail.com>
2023-05-20 11:34:40 +0300

    complete task

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def breed=(breed)
+    @breed=breed
+    end
+
+    def name
+    @name
+    end
+
+    def breed
+    @breed 
+    end
 end
+
+Fido=Dog.new
+Fido.name= "Fido"
+Fido.breed= "Fido"
+
+puts Fido.name
+puts Fido.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-
-end
+    def name=(person_names)
+        @name=person_names
+    end
+    def job=(person_jobs)
+        @job=person_jobs
+    end
+    def name
+        @name
+      end
+      def job
+        @job
+      end
+    end
+    conso=Person.new
+    conso.name="conso"
+    conso.job="Software Engineer"
+    
+    puts conso.name
+    puts conso.job
+    
